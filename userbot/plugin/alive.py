85867932c64eb79f0708bb411bf914d5d861ecc2
HITMAN | KARTIK <74542005+kartikrajofficial0773h@users.noreply.github.com>
2021-01-24 15:54:43 +0530

    Add files via upload

---
+++ userbot/plugin/alive.py
@@ -0,0 +1,80 @@
+# Thanks to Sipak bro and Aryan.. 
+# animation Idea by @ItzSipak && @Hell boy_pikachu
+# Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+import asyncio
+import os
+import requests
+import time
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import random
+from telethon import events, version
+from userbot.utils import admin_cmd, sudo_cmd
+from userbot import ALIVE_NAME, Lastupdate
+from . import dcdef
+from telethon.tl.types import ChannelParticipantsAdmins
+# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "KINGBOT"
+
+# Thanks to Sipak bro and Aryan.. 
+# animation Idea by @ItzSipak && @Hell boy_pikachu
+# Made by @hellboi_atul ....and thanks to @Crackexy for the logos...
+# Kang with credits else gay...
+# alive.py for DC(DARK COBRA)
+global ghanti
+ghanti = borg.uid
+edit_time = 5
+""" =======================CONSTANTS====================== """
+file1 = "https://telegra.ph/file/5bba42dacd463db433053.jpg"
+file2 = "https://telegra.ph/file/a47953a5ee354aff8b5ab.jpg"
+file3 = "https://telegra.ph/file/1c2fe5686f09ec3da1981.png"
+file4 = "https://telegra.ph/file/5bba42dacd463db433053.jpg"
+""" =======================CONSTANTS====================== """
+
+
+@borg.on(admin_cmd(pattern=r"alive"))
+@borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
+
+async def hmm(yes):
+    chat = await yes.get_chat()
+    global ghanti
+    ghanti = borg.uid
+    await yes.delete()
+    uptime = await dcdef.get_readable_time((time.time() - Lastupdate))
+    pm_caption = "** 😎KINGBOT IS ALWAYS ACTIVE!!😎**\n\n"
+    pm_caption += "**This is my master special userbot!\n\n"
+    pm_caption += "Status of my bot!!\n\n"
+    pm_caption += f"➾ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ** ☞ {version.__version__}\n"
+    pm_caption += "➾ **KINGBOT version** ☞ 3.8.3\n"
+    pm_caption += "➾ **PYTHON** ☞ 3.8.3\n"
+    pm_caption += "➾ **Bot status**  ☞ FULL ACESS\n"
+    pm_caption += "➾ **More info** ☞ [click here](https://t.me/King_bot_official)\n\n"
+    pm_caption += f"➾ **KingBot ᴜᴘᴛɪᴍᴇ** ☞ {uptime}\n\n"
+    pm_caption += f"➾ **My pro owner** ☞ [{DEFAULTUSER}](tg://user?id={ghanti})\n"
+    on = await borg.send_file(yes.chat_id, file=file1,caption=pm_caption)
+
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(yes.chat_id, on, file=file2) 
+
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=file3)
+
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=file3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=file2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=file1)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(yes.chat_id, ok6, file=file4)
+
+    
+
