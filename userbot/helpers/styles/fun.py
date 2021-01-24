c7da0c47ee1438035f0810ad5ee4d3bb5e3be635
HITMAN | KARTIK <74542005+kartikrajofficial0773h@users.noreply.github.com>
2021-01-24 15:40:23 +0530

    helper log

---
+++ userbot/helpers/styles/fun.py
@@ -0,0 +1,20 @@
+from userbot.functions.funts import KING_STYLE as KING_FONT_STYLE
+from userbot.functions.funts import KINGFONTS as KINGFONTS
+from userbot.functions.funts import SIMPLEKING as SIMPLEKING
+
+
+
+
+
+
+
+
+
+#Kanger
+
+
+#tEri maa ki chut
+
+
+
+#useless
